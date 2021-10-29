a57a12abe3bdf9e01743f57e0070fa169bd6507a
Quetsalkoatl <quetsalkoatl@users.noreply.github.com>
2021-10-29 17:27:10 +0200

    add user blacklist function

--- otter_bot.py
+++ otter_bot.py
@@ -3,11 +3,14 @@ import discord
 import os
 import re
 from dotenv import load_dotenv
+from os.path import exists
 
 
 class OtterClient(discord.Client):
     OTTER_EMOJI = '🦦'
     OTTER_DANCE = '<:OtterDance:892768067652841472>'
+    IGNORE_FOOTER = 'If you want me to ignore your messages type `botter ignore me`'
+    NOTICE_FOOTER = 'If you want me respond to your messages type `botter notice me`'
 
     # add more links to otter gifs to this list
     OTTER_LIST = [
@@ -65,55 +68,114 @@ class OtterClient(discord.Client):
         663311896304418825   # giveaways
     ]
 
+    EXCLUDE_USERS_FILE = "blacklist.txt"
+
+    EXCLUDE_USERS = set()
+
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.load_blacklist()
+
     async def on_message(self, message):
         l_msg = message.content.lower()
         if l_msg.startswith('botter'):
             await self.commands(l_msg[6:].strip(), message)
             return
 
-        category = message.channel.category
-        if category is not None and category.id in self.EXCLUDE_CATEGORIES:
-            return
-
         if 'other' in l_msg:
-            for ex in self.EXCLUDE_LIST:
-                if ex in l_msg:
-                    return
-            # fancy regex substitution to replace 'other' with 'otter' ignoring case
-            response = re.sub(r'((\w*)other(\w*))', r'~~\1~~ \2otter\3', message.content, flags=re.IGNORECASE)
-            await self.send_embed('Sorry to correct you', response, message, random.choice(self.OTTER_LIST))
-            return
+            if not self.is_excluded(message):
+                return await self.other_response(message, l_msg)
 
         if 'otter' in l_msg.replace(' ', ''):
-            matches = re.findall(r'o *t *t *e *r', l_msg, flags=re.IGNORECASE)
-            for match in matches:
-                if len(match) > 5:
-                    response = re.sub(r'(\w*)(o *t *t *e *r)(\w*)', r'\1**\2**\3', message.content, flags=re.IGNORECASE)
-                    await self.send_embed('Wooo! You found me!', response, message, random.choice(self.OTTER_LIST))
-                    return
+            if not self.is_excluded(message):
+                return await self.ot_ter_response(message, l_msg)
+
+    async def other_response(self, message, l_msg):
+        for ex in self.EXCLUDE_LIST:
+            if ex in l_msg:
+                return
+            # fancy regex substitution to replace 'other' with 'otter' ignoring case
+        response = re.sub(r'((\w*)other(\w*))', r'~~\1~~ \2otter\3', message.content, flags=re.IGNORECASE)
+        await self.send_embed('Sorry to correct you', response, message, random.choice(self.OTTER_LIST))
+
+    async def ot_ter_response(self, message, l_msg):
+        matches = re.findall(r'o *t *t *e *r', l_msg, flags=re.IGNORECASE)
+        for match in matches:
+            if len(match) > 5:
+                response = re.sub(r'(\w*)(o *t *t *e *r)(\w*)', r'\1**\2**\3', message.content, flags=re.IGNORECASE)
+                await self.send_embed('Wooo! You found me!', response, message, random.choice(self.OTTER_LIST))
 
     async def commands(self, content, message):
-        if 'info' in content:
-            await self.info(message)
+        if content in ["enable", "on", "notice", "notice me"]:
+            await self.remove_blacklist(message)
+        elif content in ["disable", "off", "ignore", "ignore me"]:
+            await self.add_blacklist(message)
         else:
             await self.info(message)
 
     async def info(self, message):
-        embed = self.embed('Hello', f"Hello. I'm the otter bot {self.OTTER_DANCE}")
+        status = "notice me"
+        if message.author.id in self.EXCLUDE_USERS:
+            status = "ignore me"
+        embed = self.embed('Hello', f"I'm the otter bot {self.OTTER_DANCE}\n"
+                                    f"\n"
+                                    f"**Status**: {status}\n"
+                                    f"\n"
+                                    f"**Commands:\n**"
+                                    f"If you find me annoying I'll ignore you with `botter ignore me`.\n"
+                                    f"Otterwise I'll respond to you again with `botter notice me`\n"
+                                    f"\n"
+                                    f"Remember we're all here to have fun.")
         await message.channel.send(embed=embed, reference=message)
 
-    async def send_embed(self, title, description, message, image_url=None):
-        if message.channel.id in self.EXCLUDE_CHANNELS:
-            return
-        embed = self.embed(title, description, image_url)
+    async def remove_blacklist(self, message):
+        self.EXCLUDE_USERS.remove(message.author.id)
+        self.write_blacklist()
+        embed = self.embed('Welcome back',
+                           f"I'm glad you're allowing me to respond to your messages again. {self.OTTER_DANCE}",
+                           footer=self.IGNORE_FOOTER)
+        await message.channel.send(embed=embed, reference=message)
+
+    async def add_blacklist(self, message):
+        self.EXCLUDE_USERS.add(message.author.id)
+        self.write_blacklist()
+        embed = self.embed('Bye', f"I'll ignore you until you decide otterwise. 👋\n", footer=self.NOTICE_FOOTER)
+        await message.channel.send(embed=embed, reference=message)
+
+    async def send_embed(self, title, description, message, image_url=None, footer=IGNORE_FOOTER):
+        embed = self.embed(title, description, image_url, footer)
         await message.add_reaction(self.OTTER_EMOJI)
         await message.channel.send(embed=embed, reference=message)
 
+    def is_excluded(self, message):
+        if message.channel.id in self.EXCLUDE_CHANNELS:
+            return True
+        category = message.channel.category
+        if category is not None and category.id in self.EXCLUDE_CATEGORIES:
+            return True
+        user_id = message.author.id
+        if user_id in self.EXCLUDE_USERS:
+            return True
+        return False
+
+    def load_blacklist(self):
+        if exists(self.EXCLUDE_USERS_FILE):
+            with open(self.EXCLUDE_USERS_FILE, "r") as fp:
+                for line in fp.readlines():
+                    self.EXCLUDE_USERS.add(int(line))
+
+    def write_blacklist(self):
+        with open(self.EXCLUDE_USERS_FILE, "w") as fp:
+            for value in self.EXCLUDE_USERS:
+                fp.write(str(value) + "\n")
+
     @staticmethod
-    def embed(title, description, image_url=None):
+    def embed(title, description, image_url=None, footer=None):
         embed = discord.Embed(title=title, description=description)
         if image_url is not None:
             embed.set_image(url=image_url)
+        if footer is not None:
+            embed.set_footer(text=footer)
         return embed
 
 
